3b6e7eb5f3748300235794aba88e00b185737b21
charros96 <88594194+charros96@users.noreply.github.com>
2022-02-20 17:11:20 -0800

    test1

--- num_BTC.py
+++ num_BTC.py
@@ -3,16 +3,24 @@ import math
 def num_BTC(b):
     if b < 1:
         return 0
-    reward = 50
-    total = 50
-    count = 1
-    for block in range(1,b):
-        total += reward
-        count += 1
-        if count == 210000:
-            reward = reward/2
-            count = 1
+    reward = 50.0
+    total = 0
+    count = 0
+    #for block in range(0,b):
+    #    total += reward
+    #    count += 1
+    #    if count == 210000:
+    #        reward = reward/2
+    #        count = 0
     
-    return float(total)
 
+    rem = pow(b,1,210000)
+    splits = b//210000
+    for i in range(0,int(splits)):
+        total += reward*210000
+        reward = reward/2
+
+    if rem > 0:
+        total += rem*reward
+    return float(total)
 
